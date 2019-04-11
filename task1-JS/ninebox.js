56db164d448df22b4935fce0575456fbcc058b37
zll92624@163.com <zll92624@163.com[Cssh>
2019-04-11 09:55:23 +0800

    修改JS1

--- task1-JS/ninebox.js
+++ task1-JS/ninebox.js
@@ -20,13 +20,15 @@ console.log(color());
 //随机盒子
 function box() {
     //为避免三个盒子重复，使用逻辑或||
-    while (first == second || second == third || third == first) {
+    while (first === second || second === third || third === first) {
         var first = ~~(Math.random() * 9);
         var second = ~~(Math.random() * 9);
         var third = ~~(Math.random() * 8 + 1); //等价写法
-
-        console.log(first, second, third);
     }
+
+    console.log(first, second, third);
+
+
     //将随机出来的盒子，赋值color函数出来的随机颜色
     while (one == two || two == three || three == one) {
         var one = boxGet[first].style.backgroundColor = color();
@@ -48,14 +50,14 @@ function start() {
             boxGet[j].style.backgroundColor = "orange"
         };
 
-        box()//在定时器内执行box函数，进行随机盒子+随机颜色工作
+        box() //在定时器内执行box函数，进行随机盒子+随机颜色工作
 
     }, 1000) //延时1秒
 };
 
 
 function end() {
-    clearInterval(time)//点击end关闭后，定时器被清除，停止工作
+    clearInterval(time) //点击end关闭后，定时器被清除，停止工作
     for (var j = 0; j < 9; j++) {
         boxGet[j].style.backgroundColor = "orange"
     };
